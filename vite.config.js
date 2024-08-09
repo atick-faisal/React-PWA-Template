5434e95e5b0303cddd09c27578cccc5f98998a82
Atick Faisal <atickfaisal@gmail.com>
2024-08-09 20:55:00 +0600

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            includeAssets: [
+                "favicon.ico",
+                "apple-touch-icon.png",
+                "mask-icon.svg",
+            ],
+            manifest: {
+                name: "React.js PWA",
+                short_name: "PWA",
+                description: "Template for React.js PWA",
+                theme_color: "#242424",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
+});
